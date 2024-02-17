410886352349e42abff14fb74a0794e727e6f987
saintwq <153385206+saintwq@users.noreply.github.com>
2024-02-17 17:42:12 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -22,7 +22,8 @@ local Window = Rayfield:CreateWindow({
       FileName = "reaperloaderbegins", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://raw.githubusercontent.com/saintwq/Reaper-Loader-Roblox/main/key.lua"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"https://raw.githubusercontent.com/saintwq/Reaper-Loader-Roblox/main/key.lua"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22"
+      game.StarterGui:SetCore("SendNotification", {Title="Login Sucess!"; Text="Reaper Loader"; Duration=5;})			
    }
 })
 
@@ -156,10 +157,11 @@ local TPTab = Window:CreateTab("teste sla", nil) -- Title, Image
 
 
 local TPTab = Window:CreateTab("kaue gay", nil) -- Title, Image
-local Button1 = TPTab:CreateButton({
-   Name = "Tudo dois passa nada KKKKKKKKKKKKKKKK",
-   Callback = function()
-   game.StarterGui:SetCore("SendNotification", {Title="Reaper Loader"; Text="Infinite Jump Activated!"; Duration=5;})
+local Toggle = MainTab:CreateToggle({
+   Name = "Tudo 3??",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+        print("FARMING")
    end,
 })
-
